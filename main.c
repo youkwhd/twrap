e48c7d0c3eddba0d41a933d7ab0e183f0294e6fc
youkwhd <lolywk@tutanota.com>
2022-10-16 18:55:59 +0700

    refactor: does this worth the presicion

--- main.c
+++ main.c
@@ -1,21 +1,32 @@
 #include <stdio.h>
+#include <stddef.h>
+#include <stdbool.h>
+
 #include "twrap.h"
 
-int main(int argc, char **argv)
+int main(void)
 {
     twrap_init(stdin);
 
-    for (int i = 0, count = 0; __twrap_buf->buf[i] != '\0'; i++, count++) {
-        if (__twrap_buf->buf[i] == '\n')
-            count = 0;
+    for (size_t i = 0, count = 0; __twrap_buf->buf[i] != '\0'; i++) {
+        /* ok, this is for count to be presicion. 
+         * + speed.
+         */
+        bool set_count_zero = false;
+        bool set_count_one = false;
 
-        if (__twrap_buf->buf[i] == ' ' && (count + twrap_word_length(&__twrap_buf->buf[i + 1]) > COUNT_BREAK)) {
+        if (__twrap_buf->buf[i] == '\n')
+            set_count_zero = true;
+        else if (__twrap_buf->buf[i] == ' ' && count + (twrap_word_length(&__twrap_buf->buf[i + 1]) + 1) >= COUNT_BREAK) {
             putchar('\n');
-            count = 0;
+            set_count_one = true;
             i++;
         }
 
         putchar(__twrap_buf->buf[i]);
+
+        set_count_zero ? count = 0 : count++;
+        set_count_one ? count = 1 : count;
     }
 
     twrap_free();
