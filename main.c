1044604281f6ddfa8f83a1e017e4cb3890d28bcb
youkwhd <lolywk@tutanota.com>
2022-10-16 10:51:32 +0700

    feat: alloc dynamic array

--- main.c
+++ main.c
@@ -1,34 +1,61 @@
 #include <stdio.h>
 #include <stddef.h>
+#include <stdlib.h>
 #include <ctype.h>
 #include <unistd.h>
 
 #define BUF_SIZE 4096 * 5
 
-int main(int argc, char **argv)
+char *twrap_buf;
+size_t twrap_size = 0;
+
+void twrap_buf_init()
 {
-    FILE *fp = argc >= 2 ? fopen(argv[1], "r") : stdin;
+    twrap_buf = malloc((sizeof *twrap_buf) * BUF_SIZE);
+}
 
-    /* TODO: convert number to string */
-    /* size_t line_break_count = argc <= 2 ? argv[2] : argv[1]; */
-    size_t line_break_count = 60;
+void twrap_buf_free()
+{
+    free(twrap_buf);
+}
 
-    char buf[BUF_SIZE];
-    size_t size = 0;
+void twrap_init(FILE *fp)
+{
+    twrap_buf_init();
 
     char c;
-    size_t lbreak_count = 0;
-    while ((c = fgetc(fp)) != EOF) {
-        buf[size++] = c;
-        if (lbreak_count++ == line_break_count) {
-            buf[size++] = '\n';
-            lbreak_count = 0;
+    while ((c = fgetc(fp)) != EOF)
+        twrap_buf[twrap_size++] = c;
+    twrap_buf[twrap_size - 1] = '\0';
+}
+
+void twrap_free()
+{
+    twrap_buf_free();
+}
+
+size_t twrap_word_length(const char *word)
+{
+    size_t length = 0;
+
+    for (; *word; word++, length++)
+        if (*word == ' ')
+            break;
+
+    return length;
+}
+
+int main(int argc, char **argv)
+{
+    /* FILE *fp = argc >= 2 ? fopen(argv[1], "r") : stdin; */
+    twrap_init(stdin);
+
+    for (int i = 0; twrap_buf[i] != '\0'; i++) {
+        if (twrap_buf[i] == ' ') { 
+            printf("%zu\n", twrap_word_length(twrap_buf + (i + 1)));
         }
-        if (c == '\n')
-            lbreak_count = 0;
     }
-    printf("%s", buf);
 
-
+    twrap_free();
     return 0;
 }
