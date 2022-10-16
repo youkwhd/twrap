fb9362f5eb92eeff5bdfdb991aa6f770f166c1c3
youkwhd <lolywk@tutanota.com>
2022-10-16 19:10:49 +0700

    refactor: better naming?

--- main.c
+++ main.c
@@ -9,19 +9,19 @@ int main(void)
     twrap_init(stdin);
 
     for (size_t i = 0, count = 0; __twrap_buf->buf[i] != '\0'; i++) {
-        bool set_count_zero = false;
+        bool count_reset = false;
 
         if (__twrap_buf->buf[i] == '\n')
-            set_count_zero = true;
+            count_reset = true;
         else if (__twrap_buf->buf[i] == ' ' && count + (twrap_word_length(&__twrap_buf->buf[i + 1]) + 1) >= COUNT_BREAK) {
             __twrap_buf->buf[i] = '\n';
-            set_count_zero = true;
+            count_reset = true;
         }
 
         putchar(__twrap_buf->buf[i]);
         count++;
 
-        set_count_zero ? count = 0 : count;
+        count_reset ? count = 0 : count;
     }
 
     twrap_free();
