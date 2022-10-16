ce90ca48edd1bcc480550b3e7efa529ea1875bd0
youkwhd <lolywk@tutanota.com>
2022-10-16 15:47:05 +0700

    fix: reset count for each line

--- main.c
+++ main.c
@@ -6,6 +6,9 @@ int main(int argc, char **argv)
     twrap_init(stdin);
 
     for (int i = 0, count = 0; __twrap_buf->buf[i] != '\0'; i++, count++) {
+        if (__twrap_buf->buf[i] == '\n')
+            count = 0;
+
         if (__twrap_buf->buf[i] == ' ' && (count + twrap_word_length(&__twrap_buf->buf[i + 1]) > COUNT_BREAK)) {
             putchar('\n');
             count = 0;
