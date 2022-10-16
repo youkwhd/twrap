7715f0cdeedf7fd478fe15e1dcdefe8e15da3e10
youkwhd <lolywk@tutanota.com>
2022-10-16 19:08:01 +0700

    refactor: wow, didn't thought i could think of this

--- main.c
+++ main.c
@@ -13,21 +13,18 @@ int main(void)
          * + speed.
          */
         bool set_count_zero = false;
-        bool set_count_one = false;
 
         if (__twrap_buf->buf[i] == '\n')
             set_count_zero = true;
         else if (__twrap_buf->buf[i] == ' ' && count + (twrap_word_length(&__twrap_buf->buf[i + 1]) + 1) >= COUNT_BREAK) {
-            putchar('\n');
-            set_count_one = true;
-            i++;
+            __twrap_buf->buf[i] = '\n';
+            set_count_zero = true;
         }
 
         putchar(__twrap_buf->buf[i]);
         count++;
 
         set_count_zero ? count = 0 : count;
-        set_count_one ? count = 1 : count;
     }
 
     twrap_free();
