5a677d8801a77fbe25841386b11f5feb1ce9b671
youkwhd <lolywk@tutanota.com>
2023-02-03 14:41:58 +0700

    feat: better algorithm

--- args.c
+++ args.c
@@ -6,6 +6,16 @@
 #include "args.h"
 #include "str.h"
 
+arg *args_find_arg(arg *args, size_t args_size, const char *arg_flag, arg_type arg_type)
+{
+    for (size_t i = 0; i < args_size; i++)
+        if (args[i].valid_args[arg_type] != NULL && strcmp(arg_flag, args[i].valid_args[arg_type]) == 0)
+            return args + i;
+
+    return NULL;
+}
+
+
 void args_parse_long(const int argc, char **argv, arg *args, size_t args_size)
 {
     const size_t ARG_DASH_COUNT = 2;
@@ -15,27 +25,21 @@ void args_parse_long(const int argc, char **argv, arg *args, size_t args_size)
             continue;
 
         char *val = argv[i] + ARG_DASH_COUNT;
+        arg *arg_target = args_find_arg_long(args, args_size, val);
 
-        for (size_t j = 0; j < args_size; j++) {
-            if (args[j].valid_args[1] != NULL && strcmp(val, args[j].valid_args[1]) != 0)
-                continue;
-
-            switch (args[j].arg_type) {
-            case ARG_TOGGLE:
-                /* prevents memory leaks
-                 */
-                if (*args[j].arg_value_ptr != NULL)
-                    break;
-
-                bool *tmp_ptr = malloc(sizeof *tmp_ptr);
-                *tmp_ptr = true;
-                *args[j].arg_value_ptr = (void *)tmp_ptr;
+        switch (arg_target->arg_value_type) {
+        case ARG_VALUE_BOOL_TOGGLE:
+            /* prevents memory leaks
+             */
+            if (*arg_target->arg_value_ptr != NULL)
                 break;
-            case ARG_VALUE:
-                *args[j].arg_value_ptr = argv[++i];
-                break;
-            }
 
+            bool *tmp_ptr = malloc(sizeof *tmp_ptr);
+            *tmp_ptr = true;
+            *arg_target->arg_value_ptr = (void *)tmp_ptr;
+            break;
+        case ARG_VALUE_STR:
+            *arg_target->arg_value_ptr = argv[++i];
             break;
         }
     }
@@ -59,41 +63,30 @@ void args_parse_short(const int argc, char **argv, arg *args, size_t args_size)
          */
         while (val_it < val_len) {
             const char flag[2] = {val[val_it], '\0'};
+            arg *arg_target = args_find_arg_short(args, args_size, flag);
 
-            for (size_t j = 0; j < args_size; j++) {
-                if (args[j].valid_args[0] != NULL && strcmp(flag, args[j].valid_args[0]) != 0)
-                    continue;
-
-                switch (args[j].arg_type) {
-                case ARG_TOGGLE:
-                    /* prevents memory leaks
-                     */
-                    if (*args[j].arg_value_ptr != NULL)
-                        goto endloop;
-
-                    bool *tmp_ptr = malloc(sizeof *tmp_ptr);
-                    *tmp_ptr = true, is_multiple_toggle = true;
-                    *args[j].arg_value_ptr = (void *)tmp_ptr;
+            switch (arg_target->arg_value_type) {
+            case ARG_VALUE_BOOL_TOGGLE:
+                /* prevents memory leaks
+                 */
+                if (*arg_target->arg_value_ptr != NULL)
                     break;
-                case ARG_VALUE:
-                    /* possible arg that has value 
-                     * misplaced with multiple toggle args
-                     *
-                     * this only occurs on single dash arguments
-                     */
-                    if (is_multiple_toggle)
-                        goto endloop;
 
-                    *args[j].arg_value_ptr = argv[++i];
-                    break;
-                }
-
-/* this breaks the for loop
-* not the switch statement
-*/
-endloop:
+                bool *tmp_ptr = malloc(sizeof *tmp_ptr);
+                *tmp_ptr = true, is_multiple_toggle = true;
+                *arg_target->arg_value_ptr = (void *)tmp_ptr;
                 break;
+            case ARG_VALUE_STR:
+                /* possible arg that has value 
+                 * misplaced with multiple toggle args
+                 *
+                 * this only occurs on single dash arguments
+                 */
+                if (is_multiple_toggle)
+                    break;
 
+                *arg_target->arg_value_ptr = argv[++i];
+                break;
             }
 
             val_it++;
@@ -113,7 +106,7 @@ void args_init(const int argc, char **argv, arg *args, size_t args_size)
 void args_free(arg *args, size_t args_size) 
 {
     for (size_t i = 0; i < args_size; i++)
-         if (args[i].arg_type == ARG_TOGGLE && *args[i].arg_value_ptr != NULL)
+         if (args[i].arg_value_type == ARG_VALUE_BOOL_TOGGLE && *args[i].arg_value_ptr != NULL)
              free(*args[i].arg_value_ptr);
 }
 
@@ -123,11 +116,11 @@ void __args_debug(arg *args, size_t args_size)
         if (*args[i].arg_value_ptr) {
             printf("[ARGS_DEBUG] %s -> ", *args[i].valid_args);
 
-            switch (args[i].arg_type) {
-            case ARG_VALUE:
+            switch (args[i].arg_value_type) {
+            case ARG_VALUE_STR:
                 puts((char *)*args[i].arg_value_ptr);
                 break;
-            case ARG_TOGGLE:
+            case ARG_VALUE_BOOL_TOGGLE:
                 puts(*((bool *)*args[i].arg_value_ptr) ? "<true>" : "<false>");
                 break;
             }
--- args.h
+++ args.h
@@ -3,19 +3,29 @@
 
 #include <stddef.h>
 
+#define args_find_arg_long(args, args_size, arg_flag) args_find_arg(args, args_size, arg_flag, ARG_TYPE_LONG)
+#define args_find_arg_short(args, args_size, arg_flag) args_find_arg(args, args_size, arg_flag, ARG_TYPE_SHORT)
+
+typedef enum arg_val_type {
+    ARG_VALUE_STR,
+    ARG_VALUE_BOOL_TOGGLE
+} arg_val_type;
+
 typedef enum arg_type {
-    ARG_VALUE,
-    ARG_TOGGLE
+    ARG_TYPE_SHORT,
+    ARG_TYPE_LONG
 } arg_type;
+
 
 typedef struct arg {
     char *valid_args[2];
-    arg_type arg_type;
+    arg_val_type arg_value_type;
     void **arg_value_ptr;
 } arg;
 
 void args_init(const int argc, char **argv, arg *args, size_t args_size);
 void args_free(arg *args, size_t args_size);
+arg *args_find_arg(arg *args, size_t args_size, const char *arg_flag, arg_type arg_type);
 void __args_debug(arg *args, size_t args_size);
 
 #endif
